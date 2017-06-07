05df6463b5f4f89a42a443603ffb511e2001e290
= <alvarovisiont@gmail.com>
2017-06-07 19:32:51 -0400

    Base de datos actualizada

--- proyecto_alcaldia.sql
+++ proyecto_alcaldia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-06-2017 a las 05:27:27
+-- Tiempo de generación: 08-06-2017 a las 01:31:49
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
@@ -41,7 +41,7 @@ CREATE TABLE `acceso` (
 --
 
 INSERT INTO `acceso` (`id_acceso`, `user_id`, `departamento_id`, `area_id`, `sub_area_id`, `created_at`, `updated_at`) VALUES
-(4, 1, 4, '4', '7', '2017-06-02 07:24:06', '2017-06-02 07:24:06');
+(4, 1, 1, '1,2,3,4,5', '3,4,5,6,7,8,9,10,11', '2017-06-08 02:57:52', '2017-06-08 02:57:52');
 
 -- --------------------------------------------------------
 
@@ -62,7 +62,11 @@ CREATE TABLE `area` (
 --
 
 INSERT INTO `area` (`id_area`, `departamento_id`, `nombre`, `created_at`, `updated_at`) VALUES
-(4, 4, 'Configuración', '2017-06-02 07:23:04', '2017-06-02 07:23:04');
+(1, 1, 'Departamentos', '2017-06-07 21:50:49', '2017-06-07 21:50:49'),
+(2, 1, 'Proveedores', '2017-06-07 21:52:13', '2017-06-07 21:52:13'),
+(3, 1, 'Unidades', '2017-06-07 21:54:18', '2017-06-07 21:54:34'),
+(4, 1, 'Configuración', '2017-06-07 21:54:59', '2017-06-07 21:54:59'),
+(5, 1, 'Requisiciones', '2017-06-07 21:55:17', '2017-06-07 21:55:17');
 
 -- --------------------------------------------------------
 
@@ -84,7 +88,86 @@ CREATE TABLE `com_configs` (
 --
 
 INSERT INTO `com_configs` (`id`, `ano`, `presidente`, `coordinador`, `created_at`, `updated_at`) VALUES
-(1, '2017', 'Alvaro Guedez', 'Mario Armijo', '2017-06-02 07:26:33', '2017-06-02 07:26:33');
+(1, '2017', 'Alvaro Guedez', 'Susana Rodríguez', '2017-06-07 22:29:36', '2017-06-07 22:29:36');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `com_departamentos`
+--
+
+CREATE TABLE `com_departamentos` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `programatica` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `com_departamentos`
+--
+
+INSERT INTO `com_departamentos` (`id`, `programatica`, `descripcion`, `created_at`, `updated_at`) VALUES
+(1, '11.01.00.51', 'Desarrollo Urbano', '2017-06-07 22:05:08', '2017-06-08 03:04:00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `com_provees`
+--
+
+CREATE TABLE `com_provees` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `rif_cedula` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `razon_social` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `direccion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `telefono` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `com_requisiciones`
+--
+
+CREATE TABLE `com_requisiciones` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `codigo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fecha` date NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `des_unidad` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unidad` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `centro` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ano` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `com_requisiciones`
+--
+
+INSERT INTO `com_requisiciones` (`id`, `codigo`, `descripcion`, `fecha`, `status`, `des_unidad`, `unidad`, `centro`, `ano`, `created_at`, `updated_at`) VALUES
+(1, '0003', 'Primera requisición', '2017-06-07', 'activo', 'Desarrollo Urbano', '11.01.00.51', 'centro de cagua', 2017, '2017-06-08 03:01:05', '2017-06-08 03:04:28');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `com_unidades`
+--
+
+CREATE TABLE `com_unidades` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `codigo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -106,7 +189,7 @@ CREATE TABLE `departamentos` (
 --
 
 INSERT INTO `departamentos` (`id_departamento`, `nombre`, `descripcion`, `fa_class`, `created_at`, `updated_at`) VALUES
-(4, 'Compras', 'departamento encargado de hacer las compras', 'fa fa-shopping-cart', '2017-06-02 07:22:37', '2017-06-02 07:22:37');
+(1, 'Compras', 'departamento de compras', 'fa fa-home', '2017-06-07 21:50:49', '2017-06-07 21:50:49');
 
 -- --------------------------------------------------------
 
@@ -131,7 +214,11 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (34, '2014_03_19_184732_sub_area', 1),
 (35, '2014_03_20_184120_acceso', 1),
 (36, '2017_06_02_004129_create_com_configs_table', 1),
-(37, '2018_03_20_174120_create_users_table', 1);
+(37, '2017_06_07_004848_create_com_provees_table', 1),
+(38, '2017_06_07_143345_create_com_unidades_table', 1),
+(39, '2017_06_07_162130_create_com_departamentos_table', 1),
+(40, '2017_06_07_170752_create_com_requisiciones_table', 1),
+(41, '2018_03_20_174120_create_users_table', 1);
 
 -- --------------------------------------------------------
 
@@ -152,9 +239,7 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id_rol`, `nombre`, `descripcion`, `created_at`, `updated_at`) VALUES
-(1, 'Servicio tecnico', 'servicio', '2017-06-02 07:19:03', '2017-06-02 07:19:03'),
-(2, 'Servicio tecnico', 'servicio', '2017-06-02 07:19:19', '2017-06-02 07:19:19'),
-(3, 'Servicio tecnico', 'servicio', '2017-06-02 07:19:55', '2017-06-02 07:19:55');
+(1, 'Servicio tecnico', 'servicio', '2017-06-07 21:50:49', '2017-06-07 21:50:49');
 
 -- --------------------------------------------------------
 
@@ -177,7 +262,15 @@ CREATE TABLE `sub_area` (
 --
 
 INSERT INTO `sub_area` (`id_sub_area`, `area_id`, `nombre`, `descripcion`, `ruta`, `created_at`, `updated_at`) VALUES
-(7, 4, 'configurar', 'ruta para generar la configuración del area de compras', 'Compras\\Configurar.index', '2017-06-02 07:23:43', '2017-06-02 07:23:43');
+(3, 1, 'Ver departamentos', 'sub area para ver los departamentos', 'Compras\\Departamentos.index', '2017-06-07 21:57:25', '2017-06-07 21:57:25'),
+(4, 1, 'Crear Departamento', 'Area para crear los departamentos', 'Compras\\Departamentos.create', '2017-06-07 21:57:56', '2017-06-07 21:57:56'),
+(5, 2, 'Ver proveedores', 'Area para ver los proveedores', 'Compras\\Proveedores.index', '2017-06-07 21:58:22', '2017-06-07 21:58:22'),
+(6, 2, 'Registrar Proveedor', 'Arear para crear proveedores', 'Compras\\Proveedores.create', '2017-06-07 21:58:54', '2017-06-07 21:58:54'),
+(7, 3, 'Ver unidades', 'Area para ver las unidades', 'Compras\\UnidadesMedida.index', '2017-06-07 21:59:23', '2017-06-07 21:59:23'),
+(8, 3, 'Registrar Unidad', 'Area para registrar las nidades de compra', 'Compras\\UnidadesMedida.create', '2017-06-07 22:00:10', '2017-06-07 22:00:10'),
+(9, 4, 'configurar', 'area para grabar la configuración del sistema', 'Compras\\Configurar.index', '2017-06-07 22:02:27', '2017-06-07 22:02:27'),
+(10, 5, 'Ver requisiciones', 'ruta para ver las requisiciones del sistema', 'Compras\\Requisiciones.index', '2017-06-07 22:08:11', '2017-06-07 22:08:11'),
+(11, 5, 'Crear Requisición', 'vista para crear las requisiciones de compras', 'Compras\\Requisiciones.create', '2017-06-08 02:57:34', '2017-06-08 02:57:34');
 
 -- --------------------------------------------------------
 
@@ -206,7 +299,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nombres`, `apellidos`, `nac`, `cedula`, `usuario`, `telefono`, `password`, `rol_id`, `departamento_id`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'ladarius03', 'Maureen Raynor', 'V', '21202500', 'fhernandez', '04124362753', '$2y$10$G9WX8GvGY2zDSkF7dR4Bfe4sIrM1QtxWAo3sRoDzKQ2SBNlXLqpRC', 1, 4, NULL, '2017-06-02 07:19:55', '2017-06-02 07:24:06');
+(1, 'xeichmann', 'Mrs. Aiyana Miller DDS', 'V', '21202500', 'admin', '04124362753', '$2y$10$N7ZXKxL5mQFm6gPaaUhjRuGkwmoJtW8DWA2BRvZJen4NX1GuWZ7HS', 1, 1, NULL, '2017-06-07 21:50:50', '2017-06-08 02:57:52');
 
 --
 -- Índices para tablas volcadas
@@ -229,6 +322,30 @@ ALTER TABLE `area`
 -- Indices de la tabla `com_configs`
 --
 ALTER TABLE `com_configs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `com_departamentos`
+--
+ALTER TABLE `com_departamentos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `com_provees`
+--
+ALTER TABLE `com_provees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `com_requisiciones`
+--
+ALTER TABLE `com_requisiciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `com_unidades`
+--
+ALTER TABLE `com_unidades`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -277,32 +394,52 @@ ALTER TABLE `acceso`
 -- AUTO_INCREMENT de la tabla `area`
 --
 ALTER TABLE `area`
-  MODIFY `id_area` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_area` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `com_configs`
 --
 ALTER TABLE `com_configs`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
+-- AUTO_INCREMENT de la tabla `com_departamentos`
+--
+ALTER TABLE `com_departamentos`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `com_provees`
+--
+ALTER TABLE `com_provees`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `com_requisiciones`
+--
+ALTER TABLE `com_requisiciones`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `com_unidades`
+--
+ALTER TABLE `com_unidades`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT de la tabla `departamentos`
 --
 ALTER TABLE `departamentos`
-  MODIFY `id_departamento` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_departamento` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 --
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id_rol` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_rol` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `sub_area`
 --
 ALTER TABLE `sub_area`
-  MODIFY `id_sub_area` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_sub_area` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
